f30e4d0f58f2ae7dcc7577e1c344f31cdd4ba823
angierojas02 <223017952+angierojas02@users.noreply.github.com>
2026-06-24 18:17:19 -0300

    Editing the file to replace the task array name with 'tasks'

---
+++ backend/task.js
@@ -0,0 +1,16 @@
+export let tasks = [
+    {
+        id: "012-345",
+        title: "Barrer la cocina",
+        description: "Barrer el primer y segundo piso",
+        priority: "Alta",
+        status: "Pendiente"
+    },
+    {
+        id: "678-910",
+        title: "Estudiar",
+        description: "Estudiar para los examenes parciales",
+        priority: "Alta",
+        status: "Completada"
+    }
+]
