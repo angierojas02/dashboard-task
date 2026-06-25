ad2544c3cfe14f321585ef8f4fea03c6aa3d76d2
angierojas02 <223017952+angierojas02@users.noreply.github.com>
2026-06-25 13:45:40 -0300

    Preparation of the main file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
-
-import './App.css'
+import { Target } from './components/Target'
 
 function App() {
   const [task, setTask] = useState([])
@@ -27,46 +26,38 @@ function App() {
     <>
       <div className='grid grid-cols-3 w-full'>
         {/*PENDIENTE*/}
-        <div className='shadow w-80 m-3 p-4'>
+        <div>
           {task.
           filter(tsk => tsk.status === "Pendiente")
           .map(tsk => (
-            <div key={tsk.id}>
-              <strong>{tsk.title}</strong>
-              <p>{tsk.description}</p>
-              <p><strong>Prioridad: </strong>{tsk.priority}</p>
-              <p><strong>Estado: </strong>{tsk.status}</p>
-            </div>
+            <Target key={tsk.id}
+            {...tsk}
+            />
           ))}
         </div>
 
         {/*COMPLETADO*/}
-        <div className='shadow w-80 m-3 p-4'>
+        <div>
           {task.
           filter(tsk => tsk.status === "Completada")
           .map(tsk => (
-            <div key={tsk.id}>
-              <strong>{tsk.title}</strong>
-              <p>{tsk.description}</p>
-              <p><strong>Prioridad: </strong>{tsk.priority}</p>
-              <p><strong>Estado: </strong>{tsk.status}</p>
-            </div>
+            <Target key={tsk.id}
+            {...tsk}
+            />
           ))}
         </div>
 
         {/*EN PROCESO*/}
-       <div className='shadow w-80 m-3 p-4'>
-          {task.
-          filter(tsk => tsk.status === "En proceso")
-          .map(tsk => (
-            <div key={tsk.id}>
-              <strong>{tsk.title}</strong>
-              <p>{tsk.description}</p>
-              <p><strong>Prioridad: </strong>{tsk.priority}</p>
-              <p><strong>Estado: </strong>{tsk.status}</p>
-            </div>
-          ))}
+          <div>
+            {task.
+            filter(tsk => tsk.status === "En proceso")
+            .map(tsk => (
+              <Target key={tsk.id}
+              {...tsk}
+              />
+            ))}
         </div>
+
       </div>
     </>
   )
