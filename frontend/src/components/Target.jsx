8de1882cd7689dfdf8fa46ab0203f0258110a0a4
angierojas02 <223017952+angierojas02@users.noreply.github.com>
2026-06-25 14:28:23 -0300

    Modification of the task card to add a delete button.

--- frontend/src/components/Target.jsx
+++ frontend/src/components/Target.jsx
@@ -1,11 +1,12 @@
  
- export function Target ({title, description, priority, status}) {
+ export function Target ({id, title, description, priority, status, onDelete}) {
     return (
         <div className='shadow w-80 m-4 p-4 rounded-2xl'>
             <strong>{title}</strong>
               <p>{description}</p>
               <p><strong>Prioridad: </strong>{priority}</p>
               <p><strong>Estado: </strong>{status}</p>
+              <button onClick={() => onDelete(id)} className="bg-gray-200 rounded-2xl w-fit pl-1.5 pr-1.5 pt-1 pb-1 font-bold text-xs">Eliminar tarea</button>
         </div>
     )
  }
